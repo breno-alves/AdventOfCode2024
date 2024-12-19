c515a9140d8a6af5fd729d9e17890cba9f8c9d23
Breno Alves <brenoalvesdev@gmail.com>
2024-12-19 20:45:07 -0300

    feat: day-1 min-heap

--- day1/main.go
+++ day1/main.go
@@ -2,16 +2,42 @@ package main
 
 import (
 	"bufio"
+	"container/heap"
 	"fmt"
 	"log"
 	"math"
 	"os"
-	"sort"
 	"strconv"
 	"strings"
 )
 
-func parseInput(left, right *[]float64) error {
+type MinHeap []int
+
+func (h MinHeap) Len() int {
+	return len(h)
+}
+
+func (h MinHeap) Less(i, j int) bool {
+	return h[i] < h[j]
+}
+
+func (h MinHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *MinHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *MinHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[:n-1]
+	return x
+}
+
+func parseInput(left, right *[]int) error {
 	file, err := os.Open("input")
 	if err != nil {
 		log.Println(err)
@@ -40,26 +66,33 @@ func parseInput(left, right *[]float64) error {
 		if err != nil {
 			return err
 		}
-		*right = append(*right, float64(first))
-		*left = append(*left, float64(last))
+		*right = append(*right, first)
+		*left = append(*left, last)
 	}
 	return nil
 }
 
 func main() {
-	left := new([]float64)
-	right := new([]float64)
-	err := parseInput(left, right)
+	leftInput := new([]int)
+	rightInput := new([]int)
+	err := parseInput(leftInput, rightInput)
 	if err != nil {
 		panic(err)
 	}
 
-	sort.Float64s(*right)
-	sort.Float64s(*left)
+	leftHeap := &MinHeap{}
+	*leftHeap = *leftInput
+	heap.Init(leftHeap)
+
+	rightHeap := &MinHeap{}
+	*rightHeap = *rightInput
+	heap.Init(rightHeap)
+
 	ans := float64(0)
-	for i := 0; i < len(*right); i++ {
-		diff := math.Abs((*right)[i] - (*left)[i])
-		ans += diff
+	for leftHeap.Len() > 0 {
+		left := heap.Pop(leftHeap).(int)
+		right := heap.Pop(rightHeap).(int)
+		ans += math.Abs(float64(right - left))
 	}
 	fmt.Println(ans)
 }
